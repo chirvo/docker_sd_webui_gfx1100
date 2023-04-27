e955dd68d6c2f6b819a8168d0e7e671be4e47d65
Irving A. Bermudez <bigchirv@gmail.com>
2023-04-27 07:57:39 -0400

    Use relative paths

--- run.sh
+++ run.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+if [ ! -d "./webui/models/VAE-approx" ]; then
+  mkdir -p ./webui/models/VAE-approx
+fi
+
+if [ ! -f "./webui/models/VAE-approx/model.pt" ]; then
+  cd ./webui/models/VAE-approx
+  wget https://github.com/AUTOMATIC1111/stable-diffusion-webui/blob/master/models/VAE-approx/model.pt
+  cd -
+fi
+
 /usr/bin/podman run \
   --security-opt seccomp=unconfined \
   --group-add keep-groups \
   --device /dev/kfd:/dev/kfd \
   --device /dev/dri:/dev/dri \
-  -v /home/chirvo/src/docker_sd_webui/webui/models:/srv/webui/models/ \
-  -v /home/chirvo/src/docker_sd_webui/webui/repositories:/srv/webui/repositories/ \
-  -v /home/chirvo/src/docker_sd_webui/webui/extensions:/srv/webui/extensions/ \
-  -v /home/chirvo/src/docker_sd_webui/webui/outputs:/srv/webui/outputs/ \
+  -v ./webui/models:/srv/webui/models/ \
+  -v ./webui/repositories:/srv/webui/repositories/ \
+  -v ./webui/extensions:/srv/webui/extensions/ \
+  -v ./webui/outputs:/srv/webui/outputs/ \
   -p 3000:7860 \
   -it \
   --rm \
